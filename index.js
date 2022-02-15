a10a2106d94f475234e9512633ab2bf2a03da514
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-15 11:18:38 -0800

    commit tree d7be20fd5462a5eb9187a67b88e055562349116a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(record){
+    const chiefsWin = record.find(win => win.result === "W")
+   if (chiefsWin) {
+return chiefsWin.year
+   }
+}    
+//const chiefsWin =(record) => record.find(result => result === "W")
+
+
 
